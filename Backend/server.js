7ee13fd1ecb84927660a286cf6aec30d6ad6c83e
NesrineYAH <nesrineyahoum@gmail.com>
2025-09-19 00:26:16 +0200

    Installation depencies

--- Backend/server.js
+++ Backend/server.js
@@ -7,8 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/products", require("./routes/products"));
-app.use("/api/users", require("./routes/users"));
+app.use("/data/products", require("./routes/products"));
+app.use("/data/users", require("./routes/users"));
+
+app.get("/", (req, res) => {
+  res.send("Backend en marche 🚀");
+});
 
 // Démarrer serveur
 const PORT = 5000;
