75bd857df8d8ed84cddb3627f9201b29731422a3
NesrineYAH <nesrineyahoum@gmail.com>
2025-09-22 21:58:30 +0200

    Mise en pl&ace Header component

---
+++ Frontend/src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const componentName = (props) => {
+    return (
+        <div>
+<nav>
+    <u>
+        <li>CALENDRIERS DE L'AVENT</li>
+        <li>NOUVEAUTÉS & TENDANCES</li>
+        <li>Parfum</li>
+        <li>Maquillage</li>
+        <li>Soin visage</li>
+        <li>Soin corps</li>
+        <li>Cheveux</li>
+        <li>COLLECTION Noel</li>
+
+    </u>
+</nav>
+        </div>
+    );
+};
+
+export default componentName;
+
